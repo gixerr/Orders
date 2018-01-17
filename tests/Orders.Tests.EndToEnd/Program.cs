808d1605d35dd209751cae60b787b971d220aebf
gixerr <pwardyn213@gmail.com>
2018-01-17 21:34:17 +0100

    Base architecture added.

---
+++ tests/Orders.Tests.EndToEnd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orders.Tests.EndToEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
