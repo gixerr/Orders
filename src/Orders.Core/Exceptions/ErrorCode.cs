859d56559e944dfb3955937ad2d280761af47a65
gixerr <pwardyn213@gmail.com>
2018-01-22 22:42:31 +0100

    Added error codes

--- src/Orders.Core/Exceptions/ErrorCode.cs
+++ src/Orders.Core/Exceptions/ErrorCode.cs
@@ -5,6 +5,12 @@ namespace Orders.Core.Exceptions
         public const string empty_order_name = nameof(empty_order_name);
         public const string empty_category_name = nameof(empty_category_name);
         public const string empty_item_name = nameof(empty_item_name);
+        public const string order_not_found = nameof(order_not_found);
+        public const string category_not_found = nameof(category_not_found);
         public const string item_not_found = nameof(item_not_found);
+        public const string order_already_exists = nameof(order_already_exists);
+        public const string category_already_exists = nameof(category_already_exists);
+        public const string item_already_exists = nameof(item_already_exists);
     }
+
 }
