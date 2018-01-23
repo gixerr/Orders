ca3c632968efebc61dd4c5a3445ffd48ee08c091
gixerr <pwardyn213@gmail.com>
2018-01-23 21:46:23 +0100

    Added autofac builder and aplicate it to startup class

---
+++ src/Orders.Api/Framework/AutofacContainerBuilder.cs
@@ -0,0 +1,40 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Orders.Core.Repositories;
+using Orders.Infrastructure.Mappings;
+using Orders.Infrastructure.Repositories;
+using Orders.Infrastructure.Services;
+using Orders.Infrastructure.Services.Interfaces;
+
+namespace Orders.Api.Framework
+{
+    public class AutofacContainerBuilder
+    {
+        private readonly IServiceCollection _services;
+
+        public AutofacContainerBuilder(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        public IContainer Build()
+        {
+            _services.AddMvc();
+
+            var builder = new ContainerBuilder();
+            builder.Populate(_services);
+
+            builder.RegisterInstance(AutoMapperConfig.GetMapper()).SingleInstance();
+            builder.RegisterType<InMemoryOrderRepository>().As<IOrderRepository>().SingleInstance();
+            builder.RegisterType<InMemoryCategoryRepository>().As<ICategoryRepository>().SingleInstance();
+            builder.RegisterType<InMemoryItemRepository>().As<IItemRepository>().SingleInstance();
+            builder.RegisterType<OrderService>().As<IOrderService>().InstancePerLifetimeScope();
+            builder.RegisterType<CategoryService>().As<ICategoryService>().InstancePerLifetimeScope();
+            builder.RegisterType<ItemService>().As<IItemService>().InstancePerLifetimeScope();
+
+            return builder.Build();
+        }
+    }
+}
--- src/Orders.Api/Startup.cs
+++ src/Orders.Api/Startup.cs
@@ -2,28 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Orders.Api.Framework;
 
 namespace Orders.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            ApplicationContainer = new AutofacContainerBuilder(services).Build();
+
+            return new AutofacServiceProvider(ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
