773f41219558e4ad96451fe7ae2492f8149d1216
gixerr <pwardyn213@gmail.com>
2018-01-23 21:54:24 +0100

    Cleanup startup class

--- src/Orders.Api/Startup.cs
+++ src/Orders.Api/Startup.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Orders.Api.Framework;
 
 namespace Orders.Api
@@ -22,9 +17,6 @@ namespace Orders.Api
         {
             Configuration = configuration;
         }
-
-        
-
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
