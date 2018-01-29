623e9bc51e2813289113d9f9b78f48db64859809
gixerr <pwardyn213@gmail.com>
2018-01-29 22:34:15 +0100

    CategoryServiceTests remake

--- tests/Orders.Tests/Services/CategoryServiceTests.cs
+++ tests/Orders.Tests/Services/CategoryServiceTests.cs
@@ -1,13 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoFixture;
 using AutoMapper;
 using FluentAssertions;
 using Moq;
 using Orders.Core.Domain;
 using Orders.Core.Exceptions;
 using Orders.Core.Repositories;
-using Orders.Infrastructure.Mappings;
-using Orders.Infrastructure.Repositories;
+using Orders.Infrastructure.Dtos;
 using Orders.Infrastructure.Services;
 using Xunit;
 
@@ -15,160 +16,149 @@ namespace Orders.Tests.Services
 {
     public class CategoryServiceTests
     {
+        private readonly Mock<ICategoryRepository> _categoryRepositoryMock;
+        private readonly Mock<IMapper> _mapperMock;
+        private readonly Fixture _fixture;
+
+        public CategoryServiceTests()
+        {
+            _categoryRepositoryMock = new Mock<ICategoryRepository>();
+            _mapperMock = new Mock<IMapper>();
+            _fixture = new Fixture();
+        }
+
         [Fact]
         public async Task get_all_async_should_invoke_get_all_async_on_repository()
         {
-            var categoryRepositoryMock = new Mock<ICategoryRepository>();
-            var mapperMock = new Mock<IMapper>();
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object);
 
-            var categoryService = new CategoryService(categoryRepositoryMock.Object, mapperMock.Object);
             await categoryService.GetAllAsync();
 
-            categoryRepositoryMock.Verify(x => x.GetAllAsync(), Times.Once);
+            _categoryRepositoryMock.Verify(x => x.GetAllAsync(), Times.Once);
         }
 
         [Fact]
-        public async Task get_async_by_name_should_return_existing_category_with_id_from_in_memory_repository()
+        public void get_all_async_should_throw_an_exception_when_repository_is_empty()
         {
-            var categoryRepository = new InMemoryCategoryRepository();
-            var mapper = AutoMapperConfig.GetMapper();
-            var categoryService = new CategoryService(categoryRepository, mapper);
+            _categoryRepositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync((IEnumerable<Category>)null);
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object);
 
-            var CategoryDto = await categoryService.GetAsync("Category-1");
+            Func<Task> getAllCategories = async () => await categoryService.GetAllAsync();
 
-            CategoryDto.Id.Should().NotBe(Guid.Empty);
+            var expectedExcepion = getAllCategories.ShouldThrow<OrderException>();
+            expectedExcepion.And.ErrorCode.ShouldBeEquivalentTo(ErrorCode.category_not_found);
+            expectedExcepion.And.Message.ShouldBeEquivalentTo("No categories available.");
         }
 
         [Fact]
-        public async Task get_async_by_name_should_return_existing_category_with_given_name_from_in_memory_repository()
+        public async Task get_async_by_id_should_invoke_gat_async_on_repository()
         {
-            var categoryRepository = new InMemoryCategoryRepository();
-            var mapper = AutoMapperConfig.GetMapper();
-            var categoryService = new CategoryService(categoryRepository, mapper);
+            var category = _fixture.Create<Category>();
+            var categoryDto = _fixture.Create<CategoryDto>();
+            _categoryRepositoryMock.Setup(x => x.GetAsync(category.Id)).ReturnsAsync(category);
+            _mapperMock.Setup(x => x.Map<CategoryDto>(category)).Returns(categoryDto);
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object,
+                _mapperMock.Object);
 
-            var categoryDto = await categoryService.GetAsync("Category-2");
+            var expectedCategoryDto = await categoryService.GetAsync(category.Id);
 
-            categoryDto.Name.Should().BeEquivalentTo("Category-2");
+            expectedCategoryDto.Should().NotBeNull();
+            _categoryRepositoryMock.Verify(x => x.GetAsync(category.Id), Times.Once);
+            _mapperMock.Verify(x => x.Map<CategoryDto>(category));
         }
 
         [Fact]
-        public async Task get_async_by_name_should_throw_exception_when_category_with_given_name_not_exist()
+        public void get_async_by_id_should_throw_an_exception_when_category_with_given_id_not_exist()
         {
-            var categoryRepositoryMock = new Mock<ICategoryRepository>();
-            var mapperMock = new Mock<IMapper>();
-            var categoryService = new CategoryService(categoryRepositoryMock.Object, mapperMock.Object);
+            var category = _fixture.Create<Category>();
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object);
 
-            try
-            {
-                var categoryDto = await categoryService.GetAsync("FakeCategory");
-            }
-            catch (OrderException oe)
-            {
-                oe.ErrorCode.Should().BeEquivalentTo(ErrorCode.category_not_found);
-                oe.Message.Should().BeEquivalentTo($"Category with given name 'FakeCategory' not found.");
-            }
+            Func<Task> getCategory = async () => await categoryService.GetAsync(category.Id);
+
+            var expectedExcepion = getCategory.ShouldThrow<OrderException>();
+            expectedExcepion.And.ErrorCode.ShouldBeEquivalentTo(ErrorCode.category_not_found);
+            expectedExcepion.And.Message.ShouldBeEquivalentTo($"Category with given id '{category.Id}' not found.");
         }
 
         [Fact]
-        public async Task get_async_by_id_should_return_existing_category_with_given_id_from_in_memory_repository()
+        public async Task get_async_by_name_should_invoke_get_async_on_repository()
         {
-            var categoryRepository = new InMemoryCategoryRepository();
-            var mapper = AutoMapperConfig.GetMapper();
-            var categoryService = new CategoryService(categoryRepository, mapper);
+            var category = _fixture.Create<Category>();
+            var categoryDto = _fixture.Create<CategoryDto>();
+            _categoryRepositoryMock.Setup(x => x.GetAsync(category.Name)).ReturnsAsync(category);
+            _mapperMock.Setup(x => x.Map<CategoryDto>(category)).Returns(categoryDto);
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object,
+                _mapperMock.Object);
 
-            var categoryDto = await categoryService.GetAsync("Category-6");
-            var categoryDtoById = await categoryService.GetAsync(categoryDto.Id);
+            var expectedCategoryDto = await categoryService.GetAsync(category.Name);
 
-            categoryDtoById.Id.Should().Be(categoryDto.Id);
+            expectedCategoryDto.Should().NotBeNull();
+            _categoryRepositoryMock.Verify(x => x.GetAsync(category.Name), Times.Once);
+            _mapperMock.Verify(x => x.Map<CategoryDto>(category));
         }
 
         [Fact]
-        public async Task get_async_by_id_should_throw_exception_when_category_with_given_id_not_exist()
+        public void get_async_by_name_should_throw_an_exception_when_category_with_given_name_not_exist()
         {
-            var categoryRepositoryMock = new Mock<ICategoryRepository>();
-            var mapperMock = new Mock<IMapper>();
-            var categoryService = new CategoryService(categoryRepositoryMock.Object, mapperMock.Object);
+            var category = _fixture.Create<Category>();
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object);
 
-            try
-            {
-                var categoryDto = await categoryService.GetAsync(Guid.Empty);
-            }
-            catch (OrderException oe)
-            {
-                oe.ErrorCode.Should().BeEquivalentTo(ErrorCode.category_not_found);
-                oe.Message.Should().BeEquivalentTo($"Category with given id '{Guid.Empty}' not found.");
-            }
+            Func<Task> getCategory = async () => await categoryService.GetAsync(category.Name);
+
+            var expectedExcepion = getCategory.ShouldThrow<OrderException>();
+            expectedExcepion.And.ErrorCode.ShouldBeEquivalentTo(ErrorCode.category_not_found);
+            expectedExcepion.And.Message.ShouldBeEquivalentTo($"Category with given name '{category.Name}' not found.");
         }
 
         [Fact]
         public async Task add_async_should_invoke_add_async_on_repository()
         {
-            var categoryRepositoryMock = new Mock<ICategoryRepository>();
-            var mapperMock = new Mock<IMapper>();
-            var categoryService = new CategoryService(categoryRepositoryMock.Object, mapperMock.Object);
-            var category = new Category("NewCategory");
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object);
+            var category = _fixture.Create<Category>();
 
             await categoryService.AddAsync(category.Name);
 
-            categoryRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Category>()), Times.Once);
+            _categoryRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Category>()), Times.Once);
         }
 
         [Fact]
-
-        public async Task adding_exinsting_category_to_in_memory_repository_should_thow_exception()
+        public void adding_exinsting_category_should_thow_an_exception()
         {
-            var categoryRepository = new InMemoryCategoryRepository();
-            var mapper = AutoMapperConfig.GetMapper();
-            var categoryService = new CategoryService(categoryRepository, mapper);
-            var category = new Category("Category-4");
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object);
+            var category = _fixture.Create<Category>();
+            _categoryRepositoryMock.Setup(x => x.GetAsync(category.Name)).ReturnsAsync(category);
 
-            try
-            {
-                await categoryService.AddAsync(category.Name);
-            }
-            catch (OrderException oe)
-            {
-                oe.ErrorCode.Should().BeEquivalentTo(ErrorCode.category_already_exists);
-                oe.Message.Should().BeEquivalentTo($"Category with given name '{category.Name}' already exist. Category name must be unique.");
-            }
+
+            Func<Task> addCategory = async () => await categoryService.AddAsync(category.Name);
+
+            var expectedException = addCategory.ShouldThrow<OrderException>();
+            expectedException.And.ErrorCode.ShouldBeEquivalentTo(ErrorCode.category_already_exists);
+            expectedException.And.Message.ShouldBeEquivalentTo($"Category with given name '{category.Name}' already exist. Category name must be unique.");
         }
 
         [Fact]
-        public async Task trying_to_remove_nonexisting_category_should_throw_exception()
+        public async Task remove_async_should_invoke_remove_async_on_repository()
         {
-            var categoryRepositoryMock = new Mock<ICategoryRepository>();
-            var mapperMock = new Mock<IMapper>();
-            var categoryService = new CategoryService(categoryRepositoryMock.Object, mapperMock.Object);
-            
-            try
-            {
-                await categoryService.RemoveAsync(Guid.Empty);
-            }
-            catch (OrderException oe)
-            {
-                oe.ErrorCode.Should().BeEquivalentTo(ErrorCode.category_not_found);
-                oe.Message.Should().BeEquivalentTo($"Category with given id '{Guid.Empty}' not found. Unable to remove nonexiting category.");
-            }
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object);
+            var category = _fixture.Create<Category>();
+            _categoryRepositoryMock.Setup((x => x.GetAsync(category.Id))).ReturnsAsync(category);
+
+            await categoryService.RemoveAsync(category.Id);
+
+            _categoryRepositoryMock.Verify(x => x.RemoveAsync(category.Id), Times.Once);
         }
 
         [Fact]
-        public async Task get_async_on_removed_category_should_throw_exception()
+        public void removeing_nonexisting_category_should_throw_an_exception()
         {
-            var categoryRepository = new InMemoryCategoryRepository();
-            var mapper = AutoMapperConfig.GetMapper();
-            var categoryService = new CategoryService(categoryRepository, mapper);
-            
-            var categoryDto = await categoryService.GetAsync("Category-5");
-            await categoryService.RemoveAsync(categoryDto.Id);
-            try
-            {
-                await categoryService.GetAsync("Category-5");
-            }
-            catch (OrderException oe)
-            {
-                oe.ErrorCode.Should().BeEquivalentTo(ErrorCode.category_not_found);
-                oe.Message.Should().BeEquivalentTo($"Category with given name 'Category-5' not found.");
-            }
+            var categoryService = new CategoryService(_categoryRepositoryMock.Object, _mapperMock.Object);
+            var category = _fixture.Create<Category>();
+
+            Func<Task> removeCategory = async () => await categoryService.RemoveAsync(category.Id);
+
+            var expectedException = removeCategory.ShouldThrow<OrderException>();
+            expectedException.And.ErrorCode.ShouldBeEquivalentTo(ErrorCode.category_not_found);
+            expectedException.And.Message.ShouldBeEquivalentTo($"Category with given id '{category.Id}' not found. Unable to remove nonexiting category.");
         }
     }
 }
