4f76fd069596c00f84f9de31cdcc17c76258caf8
gixerr <pwardyn213@gmail.com>
2018-03-01 21:56:26 +0100

    Added base encryption

---
+++ src/Orders.Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Orders.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool Empty(this string value)
+            => string.IsNullOrWhiteSpace(value);
+    }
+}
